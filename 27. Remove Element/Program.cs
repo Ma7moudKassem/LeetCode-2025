f7a6a8e6cfbee637dd3cdcfdee8979a0a0e4f536
Mahmoud Kassem <modykassem123@gmail.com>
2025-08-03 00:40:58 +0300

    Runtime: 0 ms | Beats 00.00% Memory: 47.01 MB | Beats 38.55%

---
+++ 27. Remove Element/Program.cs
@@ -0,0 +1,27 @@
+﻿
+int[] nums = [3,3];
+
+Console.WriteLine(RemoveElement(nums, 2));
+
+int RemoveElement(int[] nums, int val)
+{
+    int i = 0, j = 0;
+
+    while (j < nums.Length)
+    {
+        if (nums[j] != val)
+        {
+            if (i != j)
+                Swap(nums, i, j);
+            i++;
+        }
+
+        j++;
+    }
+
+    return i;
+}
+static void Swap(int[] nums, int i, int j)
+{
+    (nums[j], nums[i]) = (nums[i], nums[j]);
+}
