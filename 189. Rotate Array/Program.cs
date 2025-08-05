73363e13fd901c51e0947b31e675884e5ae1adb6
Mahmoud Kassem <mahmoud.kassem@inthekloud.com>
2025-08-05 11:36:23 +0300

    Runtime: 0 ms Beats 100.00% | Memory: 66.05 MB Beats 49.64%

---
+++ 189. Rotate Array/Program.cs
@@ -0,0 +1,32 @@
+﻿int[] nums = { 1, 2, 3, 4, 5, 6, 7 }; // {1,2,3,4,5,6,7}
+
+Rotate(nums, 6);
+
+foreach (var num in nums)
+{
+    Console.Write(num + " ");
+}
+
+void Rotate(int[] nums, int k)
+{
+    int n = nums.Length;
+    k %= n;
+    //k < n -> k
+    //k = n + x -> x
+    //k = 0 -> 0
+    //k = n -> 0
+    
+    Reverse(nums, 0, n - 1);
+    Reverse(nums, 0, k - 1);
+    Reverse(nums, k, n - 1);
+}
+
+void Reverse(int[] nums, int left, int right)
+{
+    while (left < right)
+    {
+        (nums[left], nums[right]) = (nums[right], nums[left]);
+        left++;
+        right--;
+    }
+}
