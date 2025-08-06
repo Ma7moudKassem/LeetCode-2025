fc25bdafee94f830b4c4be0c801e3377b26b19b6
Mahmoud Kassem <mahmoud.kassem@inthekloud.com>
2025-08-06 11:12:40 +0300

    Runtime: 2 ms Beats 46.76% | Memory 60.52 MB Beats 21.48%

---
+++ 121. Best Time to Buy and Sell Stock/Program.cs
@@ -0,0 +1,44 @@
+﻿
+int[] prices = [1, 3, 7, 10, 3];
+Console.WriteLine(MaxProfit(prices));
+
+//Optimal -> Time: O(N)
+int MaxProfit(int[] prices)
+{
+    int minDay = int.MaxValue;
+    int maxProfit = 0;
+    foreach (int price in prices)
+    {
+        if (price < minDay)
+            minDay = price;
+        else
+        {
+            maxProfit = Math.Max(maxProfit, price - minDay);
+        }
+    }
+   
+
+    return maxProfit;
+}
+
+//Brute-Force Time -> O(n^2)
+//int MaxProfit(int[] prices)
+//{
+//    int n = prices.Length;
+//    int maxProfit = 0, end = n - 1;
+
+//    for (int i = 0; i < n; i++)
+//    {
+//        int start = i + 1;
+//        if (start >= prices.Length)
+//            break;
+
+//        var sellDays = prices[start..];
+
+//        int profit = sellDays.Max() - prices[i];
+
+//        maxProfit = int.Max(maxProfit, profit);
+//    }
+
+//    return maxProfit;
+//}
