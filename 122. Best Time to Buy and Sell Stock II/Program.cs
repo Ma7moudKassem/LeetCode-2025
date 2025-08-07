6fec12e02f7152cf46ab254cae6c442436b7538e
Mahmoud Kassem <mahmoud.kassem@inthekloud.com>
2025-08-07 10:53:46 +0300

    Runtime 0 ms Beats 100.00% | Memory 43.40 MB Beats 91.27%

---
+++ 122. Best Time to Buy and Sell Stock II/Program.cs
@@ -0,0 +1,14 @@
+﻿
+int[] prices = [7, 1, 5, 3, 6, 4];
+Console.WriteLine(MaxProfit(prices));
+int MaxProfit(int[] prices)
+{
+    int profit = 0;
+    for (int i = prices.Length -1 ; i > 0; i--)
+    {
+        if (prices[i] > prices[i - 1])
+            profit += prices[i] - prices[i - 1];
+    }
+
+    return profit;
+}
