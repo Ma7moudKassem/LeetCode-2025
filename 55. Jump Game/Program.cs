6573f01f64c82a9949d43fd20913bc3a60a89848
Mahmoud Kassem <modykassem123@gmail.com>
2025-08-09 12:10:08 +0300

    Runtime: 2 ms Beats 64.32% | Memory: 60.60 MB Beats 97.17%

---
+++ 55. Jump Game/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] nums = [3, 2, 1, 0, 4];
+
+Console.WriteLine(CanJump(nums));
+
+bool CanJump(int[] nums)
+{
+    int maxReach = 0;
+    for (int i = 0; i < nums.Length; i++)
+    {
+        if(i > maxReach)
+            return false;
+
+        maxReach = Math.Max(maxReach, nums[i] + i);
+    }
+
+    return true;
+}
