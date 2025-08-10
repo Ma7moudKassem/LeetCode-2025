56ab390aaf1bd27a5501c8bd0842da8ce59242ea
Mahmoud Kassem <mahmoud.kassem@inthekloud.com>
2025-08-10 11:14:19 +0300

    Runtime: 0 ms | Beats 100.00% Memory: 45.76 MB | Beats 85.28%

---
+++ 45. Jump Game II/Program.cs
@@ -0,0 +1,28 @@
+﻿int[] test = [1, 0, 0, 1, 4];
+
+Console.WriteLine(Jump(test));
+
+int Jump(int[] nums)
+{
+    if (nums == null || nums.Length < 2) return 0;
+
+    int farthest = 0;
+    int jumps = 0;
+    int currentEnd = 0;
+
+    for (int i = 0; i < nums.Length - 1; i++)
+    {
+        farthest = Math.Max(farthest, nums[i] + i);
+
+        if (i == currentEnd)
+        {
+            jumps++;
+            currentEnd = farthest;
+
+            if (currentEnd >= nums.Length - 1)
+                break;
+        }
+    }
+
+    return jumps;
+}
