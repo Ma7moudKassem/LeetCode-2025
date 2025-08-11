6b114ddd3a2044d905f92c056d91702d191fc5c0
Mahmoud Kassem <mahmoud.kassem@inthekloud.com>
2025-08-11 11:13:39 +0300

    Runtime: 1 ms Beats 82.34% | Memory: 41.52 MB Beats 93.71%

---
+++ 274. H-Index/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] nums = [0, 0, 2];
+
+Console.WriteLine(HIndex(nums));
+
+int HIndex(int[] citations)
+{
+    int hIndex = 0;
+    
+    for (int i = 0; i < citations.Length; i++)
+    {
+        if (citations[i] >= i + 1)
+            hIndex++;
+        else 
+            hIndex--;
+    }
+
+    return hIndex;
+}
