a47ad17509da514c8ba8f7f6bb30700d3b19ca98
Mahmoud Kassem <modykassem123@gmail.com>
2025-08-28 10:53:51 +0300

    Runtime 0 ms Beats 100.00% | Memory 71.10 MB Beats 13.85%

---
+++ 134. Gas Station/Program.cs
@@ -0,0 +1,26 @@
+﻿
+
+int[] gas = [1, 2, 3, 4, 5], cost = [3, 4, 5, 1, 2];
+
+Console.WriteLine(CanCompleteCircuit(gas, cost));
+
+int CanCompleteCircuit(int[] gas, int[] cost)
+{
+    int totalGas = 0, totalCost = 0, tank = 0,
+    start = 0;
+    for (int i = 0; i < gas.Length; i++)
+    {
+        totalGas += gas[i];
+        totalCost += cost[i];
+
+        tank += gas[i] - cost[i];
+
+        if (tank < 0)
+        {
+            start = i + 1;
+            tank = 0;
+        }
+    }
+
+    return totalGas < totalCost ? -1 : start;
+}
