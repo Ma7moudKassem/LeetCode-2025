db9c36a07beff6f5033ff7e976f9f60d268776a7
Mahmoud Kassem <mahmoud.kassem@inthekloud.com>
2025-08-31 16:10:01 +0300

    Runtime 1 ms Beats 98.89% | Memory 49.18 MB Beats 30.37%

---
+++ 1. Two Sum/Program.cs
@@ -0,0 +1,34 @@
+﻿
+foreach (var c in TwoSum([3,2,4], 6))
+{
+    Console.Write($"{c}");
+}
+
+int[] TwoSum(int[] nums, int target)
+{
+    Dictionary<int, int> map = new Dictionary<int, int>(nums.Length);
+    for (int i = 0; i < nums.Length; i++)
+        map[target - nums[i]] = i;
+
+    for (int i = 0; i < nums.Length; i++)
+        if (map.ContainsKey(nums[i]) && map[nums[i]] != i )
+            return [i, map[nums[i]]];
+
+    return [-1, -1];
+}
+
+//Refactored Version
+//int[] TwoSum(int[] nums, int target)
+//{
+//    Dictionary<int, int> map = new Dictionary<int, int>(nums.Length);
+//    for (int i = 0; i < nums.Length; i++)
+//    {
+//        int comp = target - nums[i];
+//        if (map.TryGetValue(comp, out int index))
+//            return [index, i];
+
+//        map[nums[i]] = i;
+//    }
+
+//    return [-1, -1];
+//}
