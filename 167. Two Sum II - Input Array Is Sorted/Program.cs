b0c9cc8129136c831238db0d6884f8c0b77c7cde
Mahmoud Kassem <mahmoud.kassem@inthekloud.com>
2025-08-31 16:35:36 +0300

    Runtime 0 ms Beats 100.00% | Memory 51.21 MB Beats 7.68%

---
+++ 167. Two Sum II - Input Array Is Sorted/Program.cs
@@ -0,0 +1,23 @@
+﻿foreach (int n in TwoSum([2,7,11,15], 18))
+{
+    Console.WriteLine(n);
+}
+
+int[] TwoSum(int[] numbers, int target)
+{
+    int i = 0, j = numbers.Length - 1;
+    while (i != j)
+    {
+        int sum = numbers[i] + numbers[j];
+
+        if (sum == target)
+            return [i + 1, j + 1];
+
+        if (sum > target)
+            j--;
+        else
+            i++;
+    }
+
+    return [-1, -1];
+}
