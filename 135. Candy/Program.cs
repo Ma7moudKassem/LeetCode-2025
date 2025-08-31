91c5e05fdce9a5f42c31206b3db702449bdc82c4
Mahmoud Kassem <mahmoud.kassem@inthekloud.com>
2025-08-31 12:51:43 +0300

    Runtime 6 ms Beats 43.78% | Memory 48.29 MB Beats 68.45%

---
+++ 135. Candy/Program.cs
@@ -0,0 +1,25 @@
+﻿int[] ratings = [1, 3, 2, 2, 1];
+
+Console.WriteLine(Candy(ratings));
+int Candy(int[] ratings)
+{
+    int n = ratings.Length;
+    int[] candies = new int[n];
+
+    for (int i = 0; i < n; i++)
+        candies[i] = 1;
+
+    for (int i = 1; i < ratings.Length; i++)
+    {
+        if (ratings[i] > ratings[i - 1])
+            candies[i] = candies[i - 1] + 1;
+    }
+
+    for (int i = n - 2; i >= 0; i--)
+    {
+        if (ratings[i] > ratings[i + 1])
+            candies[i] = Math.Max(candies[i], candies[i + 1] + 1);
+    }
+
+    return candies.Sum();
+}
