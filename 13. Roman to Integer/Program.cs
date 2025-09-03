a6c0e01f5d294fe9bcac39f3719b4d398fc93cc7
Mahmoud Kassem <modykassem123@gmail.com>
2025-09-03 17:37:07 +0300

    Runtime 3 ms Beats 71.44% | Memory 51.20 MB Beats 18.04%

---
+++ 13. Roman to Integer/Program.cs
@@ -0,0 +1,41 @@
+﻿
+
+
+Console.WriteLine(RomanToInt("LVIII"));
+int RomanToInt(string s)
+{
+    Dictionary<char, int> maps = new()
+    {
+        { 'I', 1 },
+        { 'V', 5 },
+        { 'X', 10 },
+        { 'L', 50 },
+        { 'C', 100 },
+        { 'D', 500 },
+        { 'M', 1000 }
+    };
+
+    // M C M X C I V
+
+    int result = 0;
+    char c = s[0];
+    result += maps[c];
+    for (int i = 1; i < s.Length; i++)
+    {
+        char t = s[i];
+
+        if (maps[t] <= maps[c])
+            result += maps[t];
+        else
+        {
+            result -= maps[c];
+            result += maps[t] - maps[c];
+        }
+     
+        c = s[i];
+    }
+
+    return result;
+}
+
+// O(N) Time , O(1) Space
