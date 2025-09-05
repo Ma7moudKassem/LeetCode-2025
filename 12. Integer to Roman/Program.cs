fc69324f783375ba48ea21f55369b56dddbce942
Mahmoud Kassem <modykassem123@gmail.com>
2025-09-05 19:37:24 +0300

    Runtime 4 ms Beats 61.43% | Memory 46.72 MB Beats 54.74%

---
+++ 12. Integer to Roman/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+
+Console.WriteLine(IntToRoman(3749));
+string IntToRoman(int num)
+{
+    int[] values = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+    string[] romans = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
+
+    StringBuilder result = new();
+
+    for (int i = 0; i < values.Length && num > 0; i++)
+    {
+        if (num >= values[i])
+        {
+            while (num >= values[i])
+            {
+                num -= values[i];
+                result.Append(romans[i]);
+            }
+        }
+    }
+
+    return result.ToString();
+}
