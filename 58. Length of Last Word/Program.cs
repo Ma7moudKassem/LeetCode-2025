81d7f20729dfa71fccd60eeb875f7ffa69b5fe59
Mahmoud Kassem <modykassem123@gmail.com>
2025-09-05 19:38:18 +0300

    Runtime 0 ms Beats 100.00% | Memory 40.31 MB Beats 10.43%

---
+++ 58. Length of Last Word/Program.cs
@@ -0,0 +1,22 @@
+﻿
+Console.WriteLine(LengthOfLastWord("  "));
+
+int LengthOfLastWord(string s)
+{
+    if (s.Length <= 1)
+        return s.Length == 0 ? 0 : s[0] != ' ' ? 1 : 0;
+
+    int pointer = s.Length - 1, l = 0;
+    while (pointer >= 0)
+    {
+        if (s[pointer] == ' ' && l > 0)
+            return l;
+
+        if (s[pointer] != ' ')
+            l++;
+
+        pointer--;
+    }
+
+    return l;
+}
