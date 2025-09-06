866cfc1fd2d944b6bb6dacc0cd52044ada1daed5
Mahmoud Kassem <modykassem123@gmail.com>
2025-09-06 18:36:59 +0300

    Runtime 1 ms Beats 88.80% | Memory 43.68 MB Beats 33.04%

---
+++ 14. Longest Common Prefix/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine(LongestCommonPrefix(["flower", "flow", "flight"]));
+string LongestCommonPrefix(string[] strs)
+{
+    var perfix = strs[0];
+    for (int i = 1; i < strs.Length; i++)
+    {
+        var current = strs[i];
+        int j = 1;
+        while (j < current.Length && j < perfix.Length && current[j] == perfix[j])
+            j++;
+
+        perfix = perfix.Substring(0, j);
+
+        if (string.IsNullOrEmpty(perfix))
+            break;
+    }
+
+    return perfix;
+}
