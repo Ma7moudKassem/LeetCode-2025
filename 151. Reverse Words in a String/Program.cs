3ec0de2aafd98ec8158215292ceafe54a175a6f1
Mahmoud Kassem <mahmoud.kassem@inthekloud.com>
2025-09-07 22:21:21 +0300

    Runtime 4 ms Beats 41.18% | Memory 42.34 MB Beats 42.82%

---
+++ 151. Reverse Words in a String/Program.cs
@@ -0,0 +1,36 @@
+﻿
+using System.Text;
+
+Console.WriteLine(ReverseWords("  hello world  "));
+string ReverseWords(string s)
+{
+    if (string.IsNullOrWhiteSpace(s) || string.IsNullOrEmpty(s))
+        return "";
+
+    int j = s.Length - 1;
+    StringBuilder stringBuilder = new();
+    while (j >= 0)
+    {
+        while (j >= 0 && s[j] == ' ')
+            j--;
+
+        if (j < 0) break;
+
+        int i = j;
+
+        while (i >= 0 && s[i] != ' ')
+            i--;
+
+        var subStr = s.AsSpan(i + 1, j - i);
+
+        stringBuilder.Append(subStr);
+
+        stringBuilder.Append(' ');
+        j = i;
+    }
+
+    string result = stringBuilder.ToString();
+    return result.Length > 0
+        ? result[..^1]
+        : result;
+}
