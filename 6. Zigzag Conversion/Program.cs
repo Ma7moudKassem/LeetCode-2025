f55ba2068740900c3649011e4781eef0e7eca41c
Mahmoud Kassem <mahmoud.kassem@inthekloud.com>
2025-09-09 11:54:20 +0300

    Runtime 9 ms Beats 62.32% | Memory 52.17 MB Beats 55.86%

---
+++ 6. Zigzag Conversion/Program.cs
@@ -0,0 +1,32 @@
+﻿
+using System.Text;
+
+Console.WriteLine(Convert("PAYPALISHIRING", 4));
+string Convert(string s, int numRows)
+{
+    if (numRows >= s.Length || numRows < 2)
+        return s;
+
+    StringBuilder[] terms = new StringBuilder[numRows];
+
+    //initialize
+    for (int i = 0; i < numRows; i++)
+        terms[i] = new StringBuilder();
+
+    int j = 0, dir = 1;
+    foreach (var c in s)
+    {
+        terms[j].Append(c);
+
+        if (j == 0) dir = 1;
+        else if (j == numRows - 1) dir = -1;
+
+        j += dir;
+    }
+
+    StringBuilder sb = new();
+    foreach (var term in terms)
+        sb.Append(term);
+
+    return sb.ToString();
+}
