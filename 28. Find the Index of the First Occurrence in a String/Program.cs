d193153083525e67ea00b285513ed1bb07f64426
Mahmoud Kassem <mahmoud.kassem@inthekloud.com>
2025-09-10 11:18:26 +0300

    Runtime 0 ms Beats 100.00% | Memory 39.68 MB Beats 35.01%

---
+++ 28. Find the Index of the First Occurrence in a String/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine(StrStr("a", "a"));
+
+int StrStr(string haystack, string needle)
+{
+    if (needle.Length > haystack.Length)
+        return -1;
+
+    int size = needle.Length;
+    for (int i = 0; i < haystack.Length; i++)
+    {
+        if (haystack.Length - i >= size)
+        {
+            string sub = haystack.Substring(i, size);
+            if (sub == needle)
+                return i;
+        }
+    }
+
+    return -1;
+}
