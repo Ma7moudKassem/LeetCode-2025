c9b3e8dd023e2bac9a2fe2b031840e4207edf1e6
Mahmoud Kassem <mahmoud.kassem@inthekloud.com>
2025-09-11 22:27:46 +0300

    Runtime 0 ms Beats 100.00% | Memory 50.87 MB Beats 65.29%

--- 167. Two Sum II - Input Array Is Sorted/Program.cs
+++ 167. Two Sum II - Input Array Is Sorted/Program.cs
@@ -1,4 +1,4 @@
-﻿foreach (int n in TwoSum([2,7,11,15], 18))
+﻿foreach (int n in TwoSum([2, 7, 11, 15], 18))
 {
     Console.WriteLine(n);
 }
@@ -6,17 +6,33 @@
 int[] TwoSum(int[] numbers, int target)
 {
     int i = 0, j = numbers.Length - 1;
-    while (i != j)
+    while (i < j)
     {
         int sum = numbers[i] + numbers[j];
-
         if (sum == target)
-            return [i + 1, j + 1];
+            return [i, j];
 
         if (sum > target)
             j--;
-        else
-            i++;
+        else i++;
+    }
+
+    return [-1, -1];
+}
+
+//O(n) space
+int[] TwoSumWithMoreSpace(int[] numbers, int target)
+{
+    Dictionary<int, int> map = [];
+
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        int temp = target - numbers[i];
+
+        if (map.ContainsKey(numbers[i]))
+            return [i, map[numbers[i]]];
+
+        map.Add(temp, i);
     }
 
     return [-1, -1];
