12ea3c9ef14949437b1884da9ef8090fb0e76bb5
Mahmoud Kassem <mahmoud.kassem@inthekloud.com>
2025-09-11 11:56:59 +0300

    Runtime 4 ms Beats 95.12% | Memory 45.07 MB Beats 72.64%

---
+++ 125. Valid Palindrome/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine(IsPalindrome("A man, a plan, a canal: Panama"));
+
+bool IsPalindrome(string s)
+{
+    int i = 0, j = s.Length - 1;
+    while (i <= j)
+    {
+        while (i < j && !char.IsLetterOrDigit(s[j]))
+            j--;
+
+        while (i < j && !char.IsLetterOrDigit(s[i]))
+            i++;
+
+        if (char.ToLowerInvariant(s[i++]) != char.ToLowerInvariant(s[j--]))
+            return false;
+    }
+
+    return true;
+}
