5026de1a1ba27e89ec0f512ddfca9d56c8412cb7
Mahmoud Kassem <mahmoud.kassem@inthekloud.com>
2025-09-11 12:15:06 +0300

    Runtime 0 ms Beats 100.00% | Memory 41.64 MB Beats 54.11%

---
+++ 392. Is Subsequence/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine(IsSubsequence("b", "abc"));
+bool IsSubsequence(string s, string t)
+{
+    if (string.IsNullOrEmpty(s))
+        return true;
+
+    if (s.Length > t.Length)
+        return false;
+
+    int i = 0, j = 0;
+    while (i < s.Length && j < t.Length)
+    {
+        if (s[i] == t[j])
+            i++;
+
+        j++;
+    }
+
+    return i >= s.Length;
+}
