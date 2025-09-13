36b032a9e1d366ac84b93728915228958812a336
Mahmoud Kassem <mahmoud.kassem@inthekloud.com>
2025-09-13 18:12:38 +0300

    Runtime 0 ms Beats 100.00% | Memory 52.90 MB Beats 25.02%

---
+++ 209. Minimum Size Subarray Sum/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.Design;
+
+Console.WriteLine(MinSubArrayLen(7, [2, 3, 1, 2, 4, 3]));
+// 2, 4, 12, 12, 25, 25, 25, 25, 25, 26, 28, 83 
+int MinSubArrayLen(int target, int[] nums)
+{
+    int i = 0, j = 0, length = int.MaxValue;
+
+    int sum = 0;
+    while (j < nums.Length)
+    {
+        sum += nums[j];
+        while (sum >= target)
+        {
+            length = Math.Min(length, j - i + 1);
+            sum -= nums[i];
+
+            i++;
+        }
+        j++;
+    }
+
+    return length == int.MaxValue ? 0 : length;
+}
