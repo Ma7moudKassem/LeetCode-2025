eb0b9bab66191d0013689c354e7142c11f0d42f0
Mahmoud Kassem <mahmoud.kassem@inthekloud.com>
2025-09-13 03:57:03 +0300

    Runtime 1 ms Beats 100.00% | Memory 64.19 MB Beats 7.04%

---
+++ 11. Container With Most Water/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine(MaxArea([1, 12, 6, 2, 5, 4, 8, 10, 7]));
+
+int MaxArea(int[] height)
+{
+    int i = 0, j = height.Length - 1;
+
+    int area = 0;
+    while (i < j)
+    {
+        int width = j - i;
+        if (height[i] <= height[j])
+        {
+            area = Math.Max(area, (width * height[i]));
+            i++;
+        }
+        else
+        {
+            area = Math.Max(area, (width * height[j]));
+            j--;
+        }
+    }
+
+    return area;
+}
