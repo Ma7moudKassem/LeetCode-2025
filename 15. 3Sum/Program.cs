2f21b2b3ad6215fdfa0a23f23111d5fe94dd7632
Mahmoud Kassem <mahmoud.kassem@inthekloud.com>
2025-09-13 03:57:45 +0300

    Runtime 32 ms Beats 92.91% | Memory 65.97 MB Beats 63.72%

---
+++ 15. 3Sum/Program.cs
@@ -0,0 +1,38 @@
+﻿ThreeSum([-1, 0, 1, 2, -1, -4]);
+
+IList<IList<int>> ThreeSum(int[] nums)
+{
+    Array.Sort(nums);// -1, -1, 0, 1
+    IList<IList<int>> result = [];
+    for (int i = 0; i < nums.Length - 2; i++)
+    {
+        if (i > 0 && nums[i] == nums[i - 1])
+            continue;
+
+        int l = i + 1, j = nums.Length - 1;
+        int target = 0 - nums[i];
+
+        while (l < j)
+        {
+            int sum = nums[l] + nums[j];
+
+            if (sum == target)
+            { 
+                result.Add([nums[i], nums[l], nums[j]]);
+                while (l < j && nums[l] == nums[l + 1])
+                    l++;
+                while (l < j && nums[j] == nums[j - 1])
+                    j--;
+
+                l++;
+                j--;
+            }
+            else if (sum > target)
+                j--;
+            else
+                l++;
+        }
+    }
+
+    return result;
+}
