b063e142d20c4fe3adddca377cb304ae005ebf25
Mahmoud Kassem <mahmoud.kassem@inthekloud.com>
2025-09-14 11:37:56 +0300

    Runtime 6 ms Beats 57.44% | Memory 43.60 MB Beats 84.89%

---
+++ 3. Longest Substring Without Repeating Characters/Program.cs
@@ -0,0 +1,19 @@
+﻿
+Console.WriteLine(LengthOfLongestSubstring("pwwkew"));
+int LengthOfLongestSubstring(string s)
+{
+    HashSet<char> uniqueChars = [];
+
+    int length = 0;
+    int j = 0;
+    for(int i = 0; i < s.Length; i++)
+    {
+        while (uniqueChars.Contains(s[i]))
+            uniqueChars.Remove(s[j++]);
+
+        uniqueChars.Add(s[i]);
+        length = Math.Max(length, i - j + 1);
+    }
+
+    return length;
+}
