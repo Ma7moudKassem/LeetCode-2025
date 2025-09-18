bc3839481d505d0bf3518dc9f4beed82d6da9a74
Mahmoud Kassem <mahmoud.kassem@inthekloud.com>
2025-09-18 16:21:44 +0300

    Runtime 3 ms Beats 47.93% | Memory 48.74 MB Beats 62.52%

---
+++ 36. Valid Sudoku/Program.cs
@@ -0,0 +1,44 @@
+﻿char[][] boards =
+[['.', '.', '4', '.', '.', '.', '6', '3', '.'],
+ ['.', '3', '.', '.', '1', '.', '.', '.', '.'],
+ ['5', '.', '.', '.', '.', '.', '.', '9', '.'],
+ ['.', '.', '.', '5', '6', '.', '.', '.', '.'],
+ ['4', '.', '3', '.', '.', '.', '.', '.', '1'],
+ ['.', '.', '.', '7', '.', '.', '.', '.', '.'],
+ ['.', '.', '.', '5', '.', '.', '1', '.', '.'],
+ ['.', '2', '.', '.', '.', '.', '.', '.', '.'],
+ ['.', '.', '.', '.', '.', '.', '.', '.', '.']];
+
+Console.WriteLine(IsValidSudoku(boards));
+
+bool IsValidSudoku(char[][] board)
+{
+    HashSet<(char, int)> boxes = [];
+    for (int i = 0; i < 9; i++)
+    {
+        HashSet<char> rows = [];
+        HashSet<char> columns = [];
+        for (int j = 0; j < 9; j++)
+        {
+            char row = board[i][j];
+            if (row != '.')
+            {
+                if (row < '1' || row > '9' || !rows.Add(row))
+                    return false;
+
+                int boxId = (i / 3) * 3 + (j / 3);
+                if (!boxes.Add((row, boxId)))
+                    return false;
+            }
+
+            char column = board[j][i];
+            if (column != '.')
+            {
+                if (column < '1' || column > '9' || !columns.Add(column))
+                    return false;
+            }
+        }
+    }
+
+    return true;
+}
