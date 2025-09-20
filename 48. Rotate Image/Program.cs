82f4e8c21567e15acce6e319e62b5c2a11af6c7b
Mahmoud Kassem <modykassem123@gmail.com>
2025-09-20 16:21:05 +0300

    Runtime 0 ms Beats 100.00% | Memory 46.76 MB Beats 82.28%

---
+++ 48. Rotate Image/Program.cs
@@ -0,0 +1,20 @@
+﻿int[][] m =
+    [[1, 2, 3],
+     [4, 5, 6],
+     [7, 8, 9]];
+
+Rotate(ref m);
+
+void Rotate(ref int[][] matrix)
+{
+    int n = matrix.Length;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i; j < n; j++)
+            (matrix[i][j], matrix[j][i]) = (matrix[j][i], matrix[i][j]);
+    }
+
+    for (int i = 0; i < matrix.Length; i++)
+        Array.Reverse(matrix[i]);
+}
