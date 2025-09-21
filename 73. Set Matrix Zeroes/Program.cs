d333a29a062d017a2458998aadac70e12c96c0da
Mahmoud Kassem <mahmoud.kassem@inthekloud.com>
2025-09-21 12:28:41 +0300

    Runtime 3 ms Beats 49.25% | Memory 53.84 MB Beats 15.46%

---
+++ 73. Set Matrix Zeroes/Program.cs
@@ -0,0 +1,34 @@
+﻿int[][] input =
+    [[1, 1, 1], [1, 0, 1], [1, 1, 1]];
+
+SetZeroes(input);
+
+void SetZeroes(int[][] matrix)
+{
+    HashSet<int> rowsZeros = [];
+    HashSet<int> columnsZeros = [];
+    for (int i = 0; i < matrix.Length; i++)
+    {
+        for (int j = 0; j < matrix[0].Length; j++)
+        {
+            if (matrix[i][j] == 0)
+            {
+                rowsZeros.Add(i);
+                columnsZeros.Add(j);
+            }
+        }
+
+    }
+
+    foreach (var row in rowsZeros)
+    {
+        for (int i = 0; i < matrix[0].Length; i++)
+            matrix[row][i] = 0;
+    }
+
+    foreach (var column in columnsZeros)
+    {
+        for (int i = 0; i < matrix.Length; i++)
+            matrix[i][column] = 0;
+    }
+}
