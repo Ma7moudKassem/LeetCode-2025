14c8c8450f07092cbb77c72e91fe8095552cb61a
Mahmoud Kassem <mahmoud.kassem@inthekloud.com>
2025-09-21 11:35:34 +0300

    Runtime 0 ms Beats 100.00% | Memory 46.54 MB Beats 53.70%

---
+++ 54. Spiral Matrix/Program.cs
@@ -0,0 +1,54 @@
+﻿
+using System.ComponentModel;
+using System.Runtime.ExceptionServices;
+using System.Security;
+
+int[][] input =
+    [[1, 2, 3, 4],
+     [5, 6, 7, 8],
+     [9, 10, 11, 12]];
+
+SpiralOrder(input);
+
+IList<int> SpiralOrder(int[][] matrix)
+{
+    int left = 0, top = 0,
+        right = matrix[0].Length - 1,
+        down = matrix.Length - 1;
+    IList<int> result = [];
+
+    while (top <= down && left <= right)
+    {
+        //From left to right
+        for (int i = left; i <= right; i++)
+            result.Add(matrix[top][i]);
+
+        top++;
+
+        //From top to down
+        for (int i = top; i <= down; i++)
+            result.Add(matrix[i][right]);
+
+        right--;
+
+        if (top <= down)
+        {
+            //from right to left
+            for (int i = right; i >= left; i--)
+                result.Add(matrix[down][i]);
+
+            down--;
+        }
+        
+        if (left <= right)
+        {
+            //From down to top
+            for (int i = down; i >= top; i--)
+                result.Add(matrix[i][left]);
+
+            left++;
+        }
+    }
+
+    return result;
+}
