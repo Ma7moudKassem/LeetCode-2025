6cdcfcf3746369e3f39fafd2e3e7cd5b8d87ab33
Mahmoud Kassem <mahmoud.kassem@inthekloud.com>
2025-09-23 12:27:00 +0300

    Runtime 1ms Beats 97.97% ---- Memory 46.58 MB Beats 35.72%

---
+++ 383. Ransom Note/Program.cs
@@ -0,0 +1,45 @@
+﻿
+Console.WriteLine(CanConstruct("aa", "ab"));
+
+//Runtime 1ms Beats 97.97% ---- Memory 46.58 MB Beats 35.72%
+bool CanConstruct(string ransomNote, string magazine)
+{
+    int[] arr = new int[26];
+    foreach (char c in magazine)
+        arr[c - 'a']++;
+
+    foreach (char ra in ransomNote)
+    {
+        if (arr[ra - 'a'] <= 0)
+            return false;
+
+        arr[ra - 'a']--;
+    }
+
+    return true;
+}
+
+
+//Runtime 9ms Beats 48.21% ---- Memory 46.33MB Beats 55.56%
+
+//bool CanConstruct(string ransomNote, string magazine)
+//{
+//    Dictionary<char, int> magMap = [];
+//    foreach (char c in magazine)
+//    {
+//        if (!magMap.ContainsKey(c))
+//            magMap.Add(c, 0);
+
+//        magMap[c]++;
+//    }
+
+//    foreach (char ra in ransomNote)
+//    {
+//        if (!magMap.ContainsKey(ra) || magMap[ra] <= 0)
+//            return false;
+
+//        magMap[ra]--;
+//    }
+
+//    return true;
+//}
