78b4c0d973ea7b3be6ec82a60cb19f444badca78
Mahmoud Kassem <modykassem123@gmail.com>
2025-09-24 20:48:10 +0300

    Runtime 0 ms Beats 100.00% | Memory 43.96 MB Beats 46.73%

---
+++ 242. Valid Anagram/Program.cs
@@ -0,0 +1,22 @@
+﻿
+Console.WriteLine(IsAnagram("anagram", "nagaram"));
+bool IsAnagram(string s, string t)
+{
+    if (s.Length != t.Length)
+        return false;
+
+    int[] chars = new int[26];
+
+    foreach (char c in s)
+        chars[c - 'a']++;
+
+    foreach (char c in t)
+    {
+        if (chars[c - 'a'] == 0)
+            return false;
+
+        chars[c - 'a']--;
+    }
+
+    return true;
+}
