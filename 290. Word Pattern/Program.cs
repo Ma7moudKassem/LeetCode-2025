f53e63e6c2b15b83dbd95e8d9d6c01503fdb0238
Mahmoud Kassem <modykassem123@gmail.com>
2025-09-24 19:40:31 +0300

    Runtime 1 ms Beats 83.60% | Memory 41.10 MB Beats 42.61%

---
+++ 290. Word Pattern/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine(WordPattern("abba", "dog cat cat dog"));
+
+bool WordPattern(string pattern, string s)
+{
+    List<string> words = [.. s.Split(' ')];
+
+    if (pattern.Length != words.Count)
+        return false;
+
+    Dictionary<string, int> wordsMap = [];
+    int[] pMap = new int[26];
+
+    for (int i = 0; i < words.Count; i++)
+    {
+        wordsMap.TryAdd(words[i], i + 1);
+
+        int c = pattern[i] - 'a';
+        if (pMap[c] == 0)
+            pMap[c] = i + 1;
+
+        if (pMap[c] != wordsMap[words[i]])
+            return false;
+    }
+
+    return true;
+}
