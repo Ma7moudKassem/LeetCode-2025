87bffa1970e5260975d4dfee3f708da0e69d6b63
Mahmoud Kassem <mahmoud.kassem@inthekloud.com>
2025-09-26 17:31:49 +0300

    Runtime 3 ms Beats 63.60% | Memory 31.76 MB Beats 45.78%

---
+++ 202. Happy Number/Program.cs
@@ -0,0 +1,28 @@
+﻿
+Console.WriteLine(IsHappy(7));
+
+bool IsHappy(int n)
+{
+    string target = n.ToString();
+    HashSet<int> visited = [];
+    while (true)
+    {
+        int happy = 0;
+        foreach (var c in target)
+        {
+            int digit = c - '0';
+
+            happy = digit * digit;
+        }
+
+        if (happy == 1)
+            return true;
+
+        if (visited.Contains(happy))
+            return false;
+
+        visited.Add(happy);
+
+        target = happy.ToString();
+    }
+}
