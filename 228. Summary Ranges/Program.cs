057e80999233b7ee9a5d51743a31a54b6ef7869c
Mahmoud Kassem <mahmoud.kassem@inthekloud.com>
2025-09-28 14:29:17 +0300

    Runtime 2 ms Beats 65.66% | Memory 47.02 MB Beats 95.48%

---
+++ 228. Summary Ranges/Program.cs
@@ -0,0 +1,23 @@
+﻿SummaryRanges([-2147483648, -2147483647, 2147483647]);
+
+IList<string> SummaryRanges(int[] nums)
+{
+    var result = new List<string>();
+    if (nums.Length == 0) return result;
+
+    int start = nums[0];
+
+    for (int i = 1; i <= nums.Length; i++)
+    {
+        if (i == nums.Length || nums[i] != nums[i - 1] + 1)
+        {
+            int end = nums[i - 1];
+            result.Add(start == end ? $"{start}" : $"{start}->{end}");
+
+            if (i < nums.Length)
+                start = nums[i];
+        }
+    }
+
+    return result;
+}
