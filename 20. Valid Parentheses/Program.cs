d7358d3ed01c35b856b1aab28338182dd85bfe94
Mahmoud Kassem <mahmoud.kassem@inthekloud.com>
2025-10-06 11:50:28 +0300

    Runtime 2 ms Beats 84.15% | Memory 42.28 MB Beats 52.71%

---
+++ 20. Valid Parentheses/Program.cs
@@ -0,0 +1,40 @@
+﻿
+//Console.WriteLine($"char: '(' -> {(int)'('} \nchar: ')' -> {(int)')'}");
+//Console.WriteLine($"char: '[' -> {(int)'['} \nchar: ']' -> {(int)']'}");
+//Console.WriteLine($"char: '{{' -> {(int)'{'} \nchar: '}}' -> {(int)'}'}");
+
+Console.WriteLine(IsValid("(]"));
+
+bool IsValid(string s)
+{
+    if (string.IsNullOrEmpty(s) || s.Length < 2)
+        return false;
+
+    Stack<char> stack = [];
+
+    foreach (char c in s)
+    {
+        if (IsOpen(c))
+        {
+            stack.Push(c);
+            continue;
+        }
+     
+        if (stack.Count == 0 || !IsMatch(stack.Peek(), c))
+            return false;
+
+        stack.Pop();
+    }
+
+    return stack.Count == 0;
+}
+
+bool IsOpen(char c) =>
+    c is '(' or '{' or '[';
+
+bool IsMatch(char open, char close)
+{
+    return (open == '(' && close == ')') ||
+           (open == '{' && close == '}') ||
+           (open == '[' && close == ']');
+}
