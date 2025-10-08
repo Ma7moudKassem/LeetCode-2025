cffc0c1961cb3ebd740839554eb9e793a849437a
Mahmoud Kassem <mahmoud.kassem@inthekloud.com>
2025-10-08 15:55:18 +0300

    Runtime 2 ms Beats 93.40% | Memory 42.88 MB Beats 62.27%

---
+++ 71. Simplify Path/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+
+Console.WriteLine(SimplifyPath("/home/"));
+string SimplifyPath(string path)
+{
+    var terms = path.Split('/', StringSplitOptions.RemoveEmptyEntries);
+
+    Stack<string> stack = [];
+    StringBuilder sb = new();
+
+    foreach (string term in terms)
+    {
+        if (term == ".")
+            continue;
+
+        if (term == "..")
+        {
+            if (stack.Count > 0)
+                stack.Pop();
+        }
+        else
+            stack.Push(term);
+    }
+
+    var parts = stack.ToList(); 
+    for (int i = parts.Count - 1; i >= 0; i--) 
+        sb.Append($"/{parts[i]}"); 
+    
+    string result = sb.ToString(); 
+
+    return string.IsNullOrEmpty(result) ? "/" : result;
+}
