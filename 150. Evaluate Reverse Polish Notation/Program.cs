9ae7ae538bfeb80ff92dea30039e6a910c6242fe
Mahmoud Kassem <mahmoud.kassem@inthekloud.com>
2025-10-09 20:51:14 +0300

    Runtime 8 ms Beats 98.18% | Memory 44.38 MB Beats 69.74%

---
+++ 150. Evaluate Reverse Polish Notation/Program.cs
@@ -0,0 +1,50 @@
+﻿string[] tokens = ["4", "13", "5", "/", "+"];
+
+Console.WriteLine(EvalRPN(tokens));
+
+int EvalRPN(string[] tokens)
+{
+    Stack<int> stack = new();
+
+    foreach (string token in tokens)
+    {
+        if (int.TryParse(token, out int num))
+        {
+            stack.Push(num);
+        }
+        else
+        {
+            int x = stack.Pop();
+            int y = stack.Pop();
+            switch (token)
+            {
+                case "+":
+                    {
+                        int res = y + x;
+                        stack.Push(res);
+                        break;
+                    }
+                case "-":
+                    {
+                        int res = y - x;
+                        stack.Push(res);
+                        break;
+                    }
+                case "*":
+                    {
+                        int res = y * x;
+                        stack.Push(res);
+                        break;
+                    }
+                case "/":
+                    {
+                        int res = y / x;
+                        stack.Push(res);
+                        break;
+                    }
+            }
+        }
+    }
+
+    return stack.Peek();
+}
---
+++ 225. Implement Stack using Queues/Program.cs
@@ -0,0 +1,46 @@
+﻿MyStack myStack = new MyStack();
+
+myStack.Push(1);
+myStack.Push(2);
+myStack.Push(3);
+myStack.Push(4);
+int x = myStack.Top(); // return 2
+int y = myStack.Pop(); // return 2
+bool v = myStack.Empty(); // return False
+
+public class MyStack
+{
+    private Queue<int> _queue;
+    private Queue<int> _temp;
+
+    public MyStack()
+    {
+        _queue = new Queue<int>();
+        _temp = new Queue<int>();
+    }
+
+    public void Push(int x)
+    {
+        _temp.Enqueue(x);
+
+        while (_queue.Count > 0)
+            _temp.Enqueue(_queue.Dequeue());
+
+        (_queue, _temp) = (_temp, _queue);
+    }
+
+    public int Pop()
+    {
+        return _queue.Dequeue();
+    }
+
+    public int Top()
+    {
+        return _queue.Peek();
+    }
+
+    public bool Empty()
+    {
+        return _queue.Count == 0;
+    }
+}
