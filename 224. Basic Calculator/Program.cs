387f226b92b9f131632795c7258185c0dd1d1eb4
Mahmoud Kassem <mahmoud.kassem@inthekloud.com>
2025-10-11 20:02:53 +0300

    Runtime 15 ms Beats 61.33% | Memory 48.88 MB Beats 53.66%

---
+++ 224. Basic Calculator/Program.cs
@@ -0,0 +1,42 @@
+﻿
+Console.WriteLine(Calculate("(1+(4+5+2)-3)+(6+8)"));
+
+
+int Calculate(string s)
+{
+    Stack<int> stack = [];
+    int res = 0;
+    int sign = 1;
+
+    for (int i = 0; i < s.Length; i++)
+    {
+        char c = s[i];
+
+        if (char.IsDigit(c))
+        {
+            int num = 0;
+            while (i < s.Length && char.IsDigit(s[i]))
+            {
+                num = num * 10 + s[i] - '0';
+                i++;
+            }
+
+            res += num * sign;
+            i--;
+        }
+        else if (c == '+') sign = 1;
+        else if (c == '-') sign = -1;
+        else if (c == '(')
+        {
+            stack.Push(res);
+            stack.Push(sign);
+
+            res = 0;
+            sign = 1;
+        }
+        else if (c == ')')
+            res = (stack.Pop() * res) + stack.Pop();
+    }
+
+    return res;
+}
---
+++ 227. Basic Calculator II/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine(Calculate("3+2*2"));
+
+int Calculate(string s)
+{
+    int res = 0;
+    char sign = '+';
+    Stack<int> stack = [];
+
+    for (int i = 0; i < s.Length; i++)
+    {
+        char c = s[i];
+        if (char.IsDigit(c))
+            res = res * 10 + (c - '0');
+
+        if(!char.IsDigit(c) && c != ' ' || i < s.Length)
+        {
+            switch (sign)
+            {
+                case '+': stack.Push(res); break;
+                case '-': stack.Push(-res); break;
+                case '*': stack.Push(stack.Pop() * res); break;
+                case '/': stack.Push(stack.Pop() / res); break;
+            }
+
+            sign = c;
+            res = 0;
+        }
+    }
+
+    return stack.Sum();
+}
