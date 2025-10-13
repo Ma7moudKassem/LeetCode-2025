9fa154932b14775a7a536e774fa7dbd897e7b38f
Mahmoud Kassem <modykassem123@gmail.com>
2025-10-13 23:02:16 +0300

    Runtime 4 ms Beats 100.00% | Memory 66.76 MB Beats 25.00%

---
+++ 1524. Number of Sub-arrays With Odd Sum/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine(NumOfSubarrays([1, 2, 3, 4, 5, 6, 7]));
+
+int NumOfSubarrays(int[] arr)
+{
+    long even = 1, odd = 0, perfix = 0, count = 0;
+
+    long mod = (long)Math.Pow(10, 9) + 7;
+    foreach (int nums in arr)
+    {
+        perfix += nums;
+
+        if (perfix % 2 == 0)
+        {
+            even++;
+            count += odd;
+        }
+        else
+        {
+            odd++;
+            count += even;
+        }
+    }
+
+    return (int)(count % mod);
+}
