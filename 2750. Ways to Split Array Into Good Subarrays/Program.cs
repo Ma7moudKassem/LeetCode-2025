56a425cdb7ed4095b53f4936696e88f90538887c
Mahmoud Kassem <modykassem123@gmail.com>
2025-10-14 19:23:36 +0300

    Runtime 7 ms Beats 55.56% | Memory 60.97 MB Beats 22.22%

---
+++ 2750. Ways to Split Array Into Good Subarrays/Program.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+
+Console.WriteLine(NumberOfGoodSubarraySplits([0, 1, 0]));
+int NumberOfGoodSubarraySplits(int[] nums)
+{
+    long ways = 1, gap = 0, prev = -1, mod = (long)Math.Pow(10, 9) + 7;
+
+    for (int i = 0; i < nums.Length; i++)
+    {
+        if (nums[i] == 1)
+        {
+            gap = i - prev;
+
+            if (prev > -1) ways = (ways * gap) % mod;
+
+            prev = i;
+        }
+    }
+
+    if (prev == -1)
+        return 0;
+
+    return (int)(ways % mod);
+}
---
+++ 930. Binary Subarrays With Sum/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine(NumSubarraysWithSum([0,0,0,0,0], 0));
+int NumSubarraysWithSum(int[] nums, int goal)
+{
+    int sum = 0, count = 0;
+
+    Dictionary<int, int> map = [];
+    map.Add(0, 1);
+    foreach (int num in nums)
+    {
+        sum += num;
+
+        if (map.ContainsKey(sum - goal))
+            count += map[sum - goal];
+
+        if (map.ContainsKey(sum))
+            map[sum]++;
+        else map[sum] = 1;
+    }
+
+    return count;
+}
