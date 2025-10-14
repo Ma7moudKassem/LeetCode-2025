a72ef784d5068642b4ea66a4ae7b21fb2a21a076
Mahmoud Kassem <modykassem123@gmail.com>
2025-10-15 01:30:53 +0300

    Runtime 31 ms Beats 61.13% | Memory 54.03 MB Beats 28.62%

---
+++ 904. Fruit Into Baskets/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.WriteLine(TotalFruit([3, 3, 3, 1, 2, 1, 1, 2, 3, 3, 4]));
+
+int TotalFruit(int[] fruits)
+{
+    int n = fruits.Length;
+    int maxAmount = 0;
+
+    Dictionary<int, int> baskets = [];
+    int j = 0;
+    for (int i = 0; i < n; i++)
+    {
+        int fruit = fruits[i];
+        if (!baskets.TryGetValue(fruit, out _))
+            baskets[fruit] = 0;
+
+        baskets[fruit]++;
+
+        while (baskets.Count > 2)
+        {
+            int ignored = fruits[j];
+            baskets[ignored]--;
+
+            if (baskets[ignored] == 0)
+                baskets.Remove(ignored);
+
+            j++;
+        }
+
+        maxAmount = Math.Max(maxAmount, i - j + 1);
+    }
+
+    return maxAmount;
+}
