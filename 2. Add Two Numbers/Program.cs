cfe68f8860b9fc8bdbd649c032445a1c1a2ae24f
Mahmoud Kassem <mahmoud.kassem@inthekloud.com>
2025-10-15 21:39:02 +0300

    Runtime 0 ms Beats 100.00% | Memory 28.92 MB Beats 60.78%

---
+++ 2. Add Two Numbers/Program.cs
@@ -0,0 +1,38 @@
+﻿
+ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+{
+    ListNode dummyHead = new(0);
+    ListNode current = dummyHead;
+
+    int carry = 0;
+    while (l1 != null || l2 != null || carry != 0)
+    {
+        int x = (l1 != null) ? l1.val : 0;
+        int y = (l2 != null) ? l2.val : 0;
+
+        int sum = x + y  + carry;
+
+        carry = sum / 10;
+
+        ListNode newNode = new ListNode(sum % 10);
+
+        current.next = newNode;
+        current = current.next;
+
+        if (l1 != null) l1 = l1.next;
+        if (l2 != null) l2 = l2.next;
+    }
+
+    return dummyHead.next;
+}
+    
+class ListNode
+{
+    public int val;
+    public ListNode? next;
+    public ListNode(int x)
+    {
+        val = x;
+        next = null;
+    }
+}
---
+++ 371. Sum of Two Integers/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine(GetSum(100, 20));
+
+int GetSum(int a, int b)
+{
+    while (b != 0)
+    {
+        int carry = (a & b) << 1;
+
+        a ^= b;
+        b = carry;
+    }
+
+    return a;
+}
