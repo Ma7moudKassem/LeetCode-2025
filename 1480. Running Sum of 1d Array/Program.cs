92311680c6ec18ae90cf7b0bbf5459a333628dc3
Mahmoud Kassem <mahmoud.kassem@inthekloud.com>
2025-10-18 16:59:36 +0300

    Runtime 0 ms Beats 100.00% | Memory 47.58 MB Beats 38.66%

---
+++ 1480. Running Sum of 1d Array/Program.cs
@@ -0,0 +1,12 @@
+﻿
+int[] RunningSum(int[] nums)
+{
+    int n = nums.Length;
+    int[] prefix = new int[n];
+    prefix[0] = nums[0];
+
+    for (int i = 1; i < n; i++)
+        prefix[i] = prefix[i - 1] + nums[i];
+
+    return prefix;
+}
