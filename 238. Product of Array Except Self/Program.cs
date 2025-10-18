8a4f828ae1bcda3f0ddee9689635d7f88d272c93
Mahmoud Kassem <mahmoud.kassem@inthekloud.com>
2025-10-18 21:56:23 +0300

    Runtime 1 ms Beats 100.00% | Memory 66.71 MB Beats 68.03%

---
+++ 238. Product of Array Except Self/Program.cs
@@ -0,0 +1,19 @@
+﻿var res = ProductExceptSelf([1, 2, 3, 4]);
+int[] ProductExceptSelf(int[] nums)
+{
+    int n = nums.Length;
+    int[] leftPrefix = new int[n];
+
+    leftPrefix[0] = 1;
+    for (int i = 1; i < n; i++)
+        leftPrefix[i] = leftPrefix[i - 1] * nums[i - 1];
+
+    int right = 1;
+    for (int i = n - 1; i >= 0; i--)
+    {
+        leftPrefix[i] *= right;
+        right *= nums[i];
+    }
+
+    return leftPrefix;
+}
---
+++ 724. Find Pivot Index/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine(PivotIndex([-1, -1, 0, 0, -1, -1]));
+
+int PivotIndex(int[] nums)
+{
+    int n = nums.Length;
+    int total = nums.Sum();
+
+    int leftSum = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        int rightSum = total - leftSum - nums[i];
+        if (rightSum == leftSum)
+            return i;
+
+        leftSum += nums[i];
+    }
+
+    return -1;
+}
