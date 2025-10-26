ffbfe576f693c5f54aef4befccbdcb2e66b90481
Mahmoud Kassem <mahmoud.kassem@inthekloud.com>
2025-10-26 13:00:05 +0300

    Runtime 3 ms Beats 100.00% | Memory 142.38 MB Beats 41.30%

---
+++ 2043. Simple Bank System/Program.cs
@@ -0,0 +1,47 @@
+﻿public class Bank
+{
+    private long[] _balance;
+    public Bank(long[] balance)
+    {
+        _balance = balance;
+    }
+
+    public bool Transfer(int account1, int account2, long money)
+    {
+        if (!IsAccountExist(account1) || !IsAccountExist(account2) || _balance[account1 - 1] < money)
+            return false;
+
+        _balance[account1 - 1] -= money;
+        _balance[account2 - 1] += money;
+
+        return true;
+    }
+
+    public bool Deposit(int account, long money)
+    {
+        if (!IsAccountExist(account))
+            return false;
+
+        _balance[account - 1] += money;
+        return true;
+    }
+
+    public bool Withdraw(int account, long money)
+    {
+        if (!IsAccountExist(account) || _balance[account - 1] < money)
+            return false;
+
+        _balance[account - 1] -= money;
+        return true;
+    }
+
+    private bool IsAccountExist(int account)
+    {
+        int length = _balance.Length;
+
+        if (account > 0 && account <= length)
+            return true;
+
+        return false;
+    }
+}
---
+++ 3461. Check If Digits Are Equal in String After Operations I/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+
+Console.WriteLine(HasSameDigits("3902"));
+bool HasSameDigits(string s)
+{
+    while (s.Length > 2)
+        s = CombineDigits(s);
+
+    return s[0] == s[1];
+}
+
+string CombineDigits(string s)
+{
+    StringBuilder sp = new(s.Length - 1);
+    for (int i = 0; i < s.Length - 1; i++)
+    {
+        int a = s[i] - '0',
+            b = s[i + 1] - '0';
+
+        int c = (a + b) % 10;
+
+        sp.Append((char)(c + '0'));
+    }
+
+    return sp.ToString();
+}
